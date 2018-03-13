94bde1ec84647e0ec71e38dbf0a01e4bfea66a72
Trevor Seitz <trevinla@gmail.com>
2018-03-13 21:50:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(integer)
-  if integer % (2..(integer-1)) == 0
-  # for n in 2..(integer - 1)
-  #   if (integer % n) == 0
-    return false
-  end
+  # if integer % (2..(integer-1)) == 0
+  for n in 2..(integer - 1)
+    if (integer % n) == 0
+      return false
+    end
   return true
 end
